c82b3b923f7392a7300c72417fce41bc52095ff7
Jonathan Araujo <jduarte@tmns.com>
2016-09-05 13:00:40 +0200

    Service mode set to 'replicated' in order to be able to use the scale command

--- cluster_up.sh
+++ cluster_up.sh
@@ -5,6 +5,6 @@ REGISTRY_HOST=manager
 # Bring up cluster infrastructure
 vagrant up manager;vagrant up node_1;vagrant up node_2;
 
-# Deploy service (ruby app) -- Run with global option to ensure only one task per node
+# Deploy service (ruby app) -- Run with replicas to be able to use the scale command
 # Redis, Nginx and Interlock containers are run during vagrant provisioning as they don't need the entire infrastructure up
-vagrant ssh manager -c "docker service create --constraint 'node.role!=manager' --mode global --network ${OVERLAY_NETWORK} --name my-app ${REGISTRY_HOST}:5000/my-app:1.0"
+vagrant ssh manager -c "docker service create --constraint 'node.role!=manager' --replicas ${REPLICAS} --network ${OVERLAY_NETWORK} --name my-app ${REGISTRY_HOST}:5000/my-app:1.0"
